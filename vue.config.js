e4204f94d29bc1edfb1993f37176186102d2b0ff
문유정 <adw00.record@gmail.com>
2024-03-29 02:18:42 +0900

    proxy 설정 진행중

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // npm run build 타겟 디렉토리 -> 해당 경로에 vue 빌드 출력물 생성됨
+  outputDir: '../src/main/resources/static',
+
+  // npm run serve 개발 진행시에 포트가 다르기때문에 프록시 설정
+/*  devServer: {
+    proxy: 'http://localhost:8080' //서버 프로젝트 포트번호와 동일할 것
+  },*/
+
+  proxy: {
+    '/api': {
+      // '/api' 로 들어오면 포트 8080(스프링 서버)로 proxy 처리
+      target: 'http://localhost:8080',
+      changeOrigin: true // cross origin 허용 (서로 다른 포트 리소스 공유)
+    }
+  }
+
 })
