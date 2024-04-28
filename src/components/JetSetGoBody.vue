4ddbe2838fc0417869d5f49b237637860161dedc
문유정 <adw00.record@gmail.com>
2024-04-28 17:32:53 +0900

    메인화면

---
+++ src/components/JetSetGoBody.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="jetSetGoBody">
+    <h1>{{ msg }}</h1>
+    <p>
+    </p>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JetSetGoBody',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
