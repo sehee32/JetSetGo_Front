a34b135e93cf465e78042b185aab0bc359e2a716
문유정 <adw00.record@gmail.com>
2024-04-28 17:34:28 +0900

    메인화면

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import JetSetGoView from '../views/JetSetGoView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: JetSetGoView
   },
   {
     path: '/about',
