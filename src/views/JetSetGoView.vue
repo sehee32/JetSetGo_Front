3f79075cf0546aeb305597a209837893ff9683ad
문유정 <adw00.record@gmail.com>
2024-04-28 17:33:55 +0900

    메인화면

---
+++ src/views/JetSetGoView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <JetSetGoBody msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import JetSetGoBody from "@/components/JetSetGoBody";
+
+export default {
+  name: 'JetSetGoView',
+  components: {
+    JetSetGoBody
+  }
+}
+</script>
