c371d28bc5d2edfc16618970dd776330b0d9e99d
sehee320 <psh0_320@naver.com>
2024-05-20 20:23:54 +0900

    meterial import설정

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './JetSetGoMain.vue'
 import router from './router'
 import VueMaterialKit from './theme/vue-material-kit-master/src/material-kit.js';
+import './theme/vue-material-kit-master/src/assets/scss/material-kit.scss';
 
 
 // createApp(App).use(router).mount('#app')
