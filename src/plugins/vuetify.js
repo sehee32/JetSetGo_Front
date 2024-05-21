cdf23426ddffe7aea74f8b0412d58acb89b7c329
YuRiLee <ddf310@naver.com>
2024-05-22 01:00:34 +0900

    YR(V0.9) 다시 내려 받고 vuetify.js 필요 코드 수정 및 동작 확인

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,8 +3,12 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { VDateInput } from 'vuetify/labs/VDateInput';
 
 export default createVuetify({
-    components,
+    components: {
+        ...components,
+        VDateInput,
+    },
     directives,
-})
+});
