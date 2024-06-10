7ea4f70bbed9ea3c6fd6e6e8a0a6dfffa73188d4
sehee320 <psh0_320@naver.com>
2024-06-10 17:00:14 +0900

    로그인페이지 수정

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,88 +1,115 @@
 <template>
-  <section class="login-section">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-6">
-          <div class="card shadow-lg">
-            <div class="card-body p-10">
-              <h2 class="text-center">LogIn</h2>
-              <p></p>
-              <form @submit.prevent="loginpage">
-                <div>
-                  <input type="text" id="username" class="form-control" v-model="username" placeholder="아이디" required />
-                </div>
-                <div>
-                  <input type="password" id="password" class="form-control" v-model="password" placeholder="비밀번호" required />
-                </div>
-                <div class="d-flex">
-                  <button type="submit" class="btn">로그인</button>
-                  <router-link to="/signup" type="button" class="btn">회원가입</router-link>
-                  <router-view/>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <div>
+    <v-img
+        class="mx-auto my-6"
+        max-width="300"
+        :src="require('@/assets/JetSetGoLogo.png')"
+    ></v-img>
+    <v-card class="custom-card mx-auto px-6 py-8">
+      <v-form
+          v-model="formValid"
+          @submit.prevent="onSubmit"
+      >
+        <v-text-field
+            v-model="username"
+            :readonly="loading"
+            :rules="[required]"
+            class="mb-2"
+            label="ID"
+            variant="outlined"
+            prepend-inner-icon="mdi-account-outline"
+            clearable
+        ></v-text-field>
+
+        <v-text-field
+            v-model="password"
+            :readonly="loading"
+            :rules="[required]"
+            :type="show ? 'text' : 'password'"
+            class="mb-2"
+            label="Password"
+            variant="outlined"
+            prepend-inner-icon="mdi-lock-outline"
+            clearable
+        >
+          <!-- 비밀번호 표시/숨김 아이콘 -->
+          <template v-slot:append-inner>
+            <v-icon @click="show = !show">
+              {{ show ? 'mdi-eye' : 'mdi-eye-off' }}
+            </v-icon>
+          </template>
+        </v-text-field>
+
+        <br>
+
+        <v-btn
+            :disabled="!formValid"
+            :loading="loading"
+            color="#00256c"
+            size="large"
+            type="submit"
+            variant="elevated"
+            block
+            @click="login"
+        >
+          로그인
+        </v-btn>
+        <br>
+        <!-- 회원가입 버튼 -->
+        <v-btn
+            size="large"
+            color="#00256c"
+            block
+            @click="goToSignUpPage"
+        >
+          회원가입
+        </v-btn>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'LoginPage',
-    components: {},
+  name: 'LoginPage',
   data() {
     return {
       username: '',
       password: '',
+      formValid: false,
+      loading: false,
+      show: false, // 비밀번호 보기/숨기기 아이콘 상태
+      required: v => !!v || '이 항목을 입력하지 않았습니다.'
     };
   },
-    setup() {},
-    created() {},
-    mounted() {},
-    unmounted() {},
-    methods: {
-      loginpage() {
-        console.log('회원가입 정보:', this.username, this.password);
-      }
+  methods: {
+    onSubmit() {
+      console.log('회원가입 정보:', this.username, this.password);
+      this.loading = true;
+
+      // 예시로, 2초 후에 로딩을 멈추도록 설정
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    },
+    login() {
+      // this.$router.push('/jetsetgomain');
+      // 로그인 버튼 누르고 난 후
+    },
+    goToSignUpPage() {
+      this.$router.push('/signup');
     }
+  }
 }
 </script>
 
-
 <style scoped>
-.login-section {
-  /*로그인 섹션 배경색이랑 패딩 설정*/
-  background-color: #f2f3f8;
-  padding: 200px 0;
-}
-.card {
-  /*카드 부분 설정*/
+.custom-card {
   border: none;
   border-radius: 15px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  margin-top: 50px;
-  margin-bottom: 138px;
+  background-color: white;
+  max-width: 600px;
+  width: 100%;
 }
-input.form-control {
-  /*인풋 창 설정*/
-  border-radius: 8px;
-  padding: 12px;
-  margin-bottom: 20px;
-  position: center;
-
-
-}
-input.form-control:focus {
-  /*포커스가 된 인풋 창 설정*/
-  box-shadow: 0 0 0 0.15rem rgba(0, 123, 255, 0.25);
-}
-
-.d-flex {
-  /*플렉스 디스플레이 사용하여 로그인,회원가입 버튼 설정*/
-  padding: 20px;
-
-}
-
 </style>
