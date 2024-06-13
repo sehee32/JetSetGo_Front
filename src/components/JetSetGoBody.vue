0a2f0442988e006576b221829f6cfd28be1bb8f1
sehee320 <psh0_320@naver.com>
2024-06-13 16:17:42 +0900

    메인화면 예매하기 틀

--- src/components/JetSetGoBody.vue
+++ src/components/JetSetGoBody.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="jetSetGoBody">
     <h1>{{ msg }}</h1>
-    <p>
-      예매하기
-    </p>
-    <div class="box-container">
-      <div class="box">운항정보</div>
-      <div class="box">출발도시 도착도시 가는날 오는날 인원수 검색</div>
-      <div class="box">인기 여행지</div>
-    </div>
-    <ul>
-      <li></li>
-    </ul>
-
   </div>
 </template>
 
@@ -31,17 +19,5 @@ export default {
 
 }
 
-.box-container {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
 
-.box {
-  width: 1000px;
-  height: 100px;
-  width: 80%;
-  background-color: #CEE3F6;
-  margin: 0 auto; /* 가운데 정렬*/
-}
 </style>
--- src/views/JetSetGoView.vue
+++ src/views/JetSetGoView.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
-    <JetSetGoBody msg="JetSetGo에서 여행을 떠나보세요"/>
+    <br><br>
+    <h2> JetSetGo에서 여행을 떠나보세요</h2>
+    <v-container><BookingPage /></v-container>
+    <JetSetGoBody></JetSetGoBody>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import BookingPage from "@/views/BookingPage";
 import JetSetGoBody from "@/components/JetSetGoBody";
 
 export default {
   name: 'JetSetGoView',
   components: {
+    BookingPage,
     JetSetGoBody
   }
 }
