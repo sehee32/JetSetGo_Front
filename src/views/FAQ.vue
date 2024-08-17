73e38d807261d3b7e521d535cb17568b6b9e19cc
YuRiLee <ddf310@naver.com>
2024-08-18 01:41:51 +0900

    YR(V6.7) FAQ - 내용 수정

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -7,66 +7,110 @@
           <div>
             <button class="faq-title" @click="faqButton1" :aria-expanded="faqContent1.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>예약했는데 항공원 구매는 어디에서 할 수 있나요?</span>
             </button>
           </div>
           <div v-if="faqContent1" class="faq-content">
-            답변 내용 입니다.
+            아래 방법을 통해 고객님의 예약에 대한 구매(결제)를 할 수 있습니다.
+            <br><br>
+            - 항공사를 통해 예약하였으나 홈페이지를 통한 구매(결제)가 불가한 경우, 서비스센터로 문의해 주세요.
+            <br><br>
+            - 여행사를 통해 예약하신 경우, 해당 여행사로 문의해 주세요.
           </div>
         </li>
         <li class="faq-item">
           <div>
             <button class="faq-title" @click="faqButton2" :aria-expanded="faqContent2.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>항공권은 꼭 순서대로 사용해야 하나요?</span>
             </button>
           </div>
           <div v-if="faqContent2" class="faq-content">
-            답변 내용 입니다.
+            항공권은 반드시 항공권에 명시된 구간의 순서대로 사용해야 합니다.
+            <br><br>
+            단, 국내선 항공권의 경우 2구간 이내로 발권된 항공권에 한해 역순 사용이 허용되며 잔여 쿠폰은 재발행 또는 환불 처리합니다.
           </div>
         </li>
         <li class="faq-item">
           <div>
             <button class="faq-title" @click="faqButton3" :aria-expanded="faqContent3.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>수하물이 파손되었는데 어떻게 해야 하나요?</span>
             </button>
           </div>
           <div v-if="faqContent3" class="faq-content">
-            답변 내용 입니다.
+            수하물이 파손되었을 경우, 수하물을 수취한 날로부터 7일 이내에 항공사에 신고하셔야 합니다.
+            <br><br>
+            여러 항공사를 이용한 경우 최종 운송 항공사로 문의하시기 바랍니다.
           </div>
         </li>
         <li class="faq-item">
           <div>
             <button class="faq-title" @click="faqButton4" :aria-expanded="faqContent4.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>개인 휴대 전자 기기에 장착된 리튬 배터리를 가져갈 수 있나요?</span>
             </button>
           </div>
           <div v-if="faqContent4" class="faq-content">
-            답변 내용 입니다.
+            개인 휴대 전자기기에 장착된 리튬 배터리는 휴대 및 위탁 모두 가능합니다.
+            <br><br>
+            • 배터리 용량(개당) 별 제한 사항
+            <br><br>
+            - 100Wh(2g) 이하 : 1인 15개 제한 (휴대+위탁)
+            <br><br>
+            - 100Wh(2g) 초과 ~ 160Wh(8g) 이하 : 항공사 승인 필요
+            단, 2g 초과 ~ 8g 이하 용량의 리튬 메탈/합금 배터리는 휴대용 의료 기기만 허용
+            <br><br>
+            - 160Wh(8g) 초과 : 운송 불가
+            <br><br>
+            * 항공사 승인 : 개인 사용 용도, 전원 OFF, 배터리가 서로 닿지 않도록 포장 등 안전 조치 적절성을 확인하는 절차
+            <br><br><br>
+            • 기타 제한 사항
+            <br><br>
+            - 배터리 분리가 불가한 헤어컬(고데기) : 운송 불가 (일본 출발편 한정)
+            <br><br>
+            - 리튬 배터리가 분리되지 않는 전동 휠, 스마트 가방 : 운송 불가
           </div>
         </li>
         <li class="faq-item">
           <div>
             <button class="faq-title" @click="faqButton5" :aria-expanded="faqContent5.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>전자담배 가져갈 수 있나요?</span>
             </button>
           </div>
           <div v-if="faqContent5" class="faq-content">
-            답변 내용 입니다.
+            리튬 배터리를 포함한 전자담배는 기내 휴대는 가능하나, 위탁 수하물로 운송은 불가합니다.
+            <br><br>
+            - 배터리 용량은 리튬금속/합금인 경우 2g, 리튬이온인 경우 100Wh 이하로 제한됩니다.
+            <br><br>
+            - 기내에서 충전은 불가하며, 기내 휴대 시 여분의 배터리는 서로 닿지 않도록 개별 포장해 주세요.
+            <br><br>
+            - 일부 국가에서는 현지 제한 규정에 따라 전자담배 관련 물품(기계, 용액 등)의 해외 반입을 전면 금지하고 있으니 여행지 국가의 전자담배 허용 여부를 사전에 확인하여 주세요.
+            <br><br>
+            아래 [운송 제한 물품]에서 반입이 제한되는 물품에 대해 추가 확인이 가능합니다.
           </div>
         </li>
         <li class="faq-item">
           <div>
             <button class="faq-title" @click="faqButton6" :aria-expanded="faqContent6.toString()">
               <span class="ico-q">Q</span>
-              <span>무엇을 질문 할까요?</span>
+              <span>보조(여분) 리튬 배터리 가져갈 수 있나요?</span>
             </button>
           </div>
           <div v-if="faqContent6" class="faq-content">
-            답변 내용 입니다.
+            개인 사용 용도의 보조(여분) 리튬 배터리는 제한적으로 기내 휴대 수하물로 운송 가능하나 위탁수하물로 운송은 불가합니다.
+            <br><br>
+            • 배터리 용량(개당) 별 제한 사항
+            <br><br>
+            - 100Wh(2g) 이하 : 1인 20개 제한, 6개 이상 항공사 승인 필요
+            <br><br>
+            - 100Wh(2g) 초과 ~ 160Wh(8g) 이하 : 항공사 승인 필요, 1인 2개 제한
+            단, 2g 초과 ~ 8g 이하 용량의 리튬 메탈/합금 배터리는 ‘휴대용 의료 기기’용 만 허용
+            <br><br>
+            - 160Wh(8g) 초과 : 운송 불가
+            <br><br>
+            • 여분의 배터리들이 서로 닿지 않도록 개별 포장 필요하며, 배터리 용량이 표시되지 않았거나 확인이 불가한 경우 운송 불가(충전용 배터리 포함)
           </div>
         </li>
       </ul>
