904e8d259c504e4264ca2a35bdb58104606bd2bd
sehee320 <psh0_320@naver.com>
2025-01-01 17:07:01 +0900

    항공편 예약 정보가 있을 시 BookingDetail페이지로 리다이렉트

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,3 +1,5 @@
+<!--LogingPage.vue-->
+
 <template>
   <div>
     <v-img
@@ -105,8 +107,27 @@ export default {
         // Vuex 액션 호출
         await this.login({ username: this.username, password: this.password });
 
-        // 메인페이지로 이동
-        this.$router.push('/bookingpage');
+        // 저장된 예약 정보가 있는지 확인
+        const bookingData = this.$store.getters.getBookingData;
+        if (bookingData) {
+          // 예약 정보가 있으면 BookingDetail 페이지로 리다이렉트
+          this.$router.push({
+            name: 'BookingDetail',
+            query: {
+              outgoingFlight: JSON.stringify(bookingData.outgoingFlight),
+              returnFlight: JSON.stringify(bookingData.returnFlight),
+              adults: bookingData.adults,
+              children: bookingData.children,
+              travelClass: bookingData.travelClass,
+              totalPrice: bookingData.totalPrice
+            }
+          });
+          // 사용한 예약 정보 초기화
+          this.$store.commit('clearBookingData');
+        } else {
+          // 예약 정보가 없으면 메인 페이지로 이동
+          this.$router.push('/bookingpage');
+        }
       } catch (error) {
         console.error('로그인 오류:', error);
         alert('아이디 또는 비밀번호를 확인해주세요.');
